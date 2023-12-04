b37e44f16e3cf09fac385a4f3378d9b173001541
Trần Minh Quân <118247596+minhquantb00@users.noreply.github.com>
2023-12-04 19:32:21 +0700

    init

---
+++ BackEnd/spring_bds/src/main/java/com/example/spring_bds/SpringBdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_bds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBdsApplication.class, args);
+	}
+
+}
